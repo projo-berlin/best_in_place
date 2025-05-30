281a413cd421e03757987c612baa8e175a26b700
Michael Emhofer <michael.emhofer@projo.berlin>
2025-05-30 15:18:54 +0200

    Don’t output server starting message in between tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -29,4 +29,5 @@ RSpec.configure do |config|
 
   Capybara.default_driver = :selenium_chrome_headless
   Capybara.javascript_driver = :selenium_chrome_headless
+  Capybara.server = :puma, { Silent: true }
 end

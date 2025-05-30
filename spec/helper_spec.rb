c2377ff893cc16cf4f6bd1fe7668baa975fcdf11
Michael Emhofer <michael.emhofer@projo.berlin>
2025-05-30 15:11:56 +0200

    Adapt test to changed rails behavior

--- spec/helper_spec.rb
+++ spec/helper_spec.rb
@@ -336,7 +336,7 @@ describe BestInPlace::Helper, type: :helper do
       end
 
       it "should render the date as text" do
-        expect(@span.text).to eq(@user.birth_date.to_date.to_s)
+        expect(@span.text).to eq(@user.birth_date.to_s)
       end
 
       it "should have a date data-bip-type" do

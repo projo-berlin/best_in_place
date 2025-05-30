6bd2ff613c9c17ee97bd106ae96cd5f1343bd230
Michael Emhofer <michael.emhofer@projo.berlin>
2025-05-30 15:17:17 +0200

    Adapt helper to new Rails Deprecation API

--- spec/helper_spec.rb
+++ spec/helper_spec.rb
@@ -32,7 +32,7 @@ describe BestInPlace::Helper, type: :helper do
     end
 
     it "should show deprecation warning" do
-      expect(ActiveSupport::Deprecation).to receive(:warn).with("[Best_in_place] :path is deprecated in favor of :url ")
+      expect(ActiveSupport::Deprecation).to receive(:new).with("[Best_in_place] :path is deprecated in favor of :url ")
 
       helper.best_in_place @user, :name, path: "http://example.com"
     end
